9ffece1b357119ed49b7ccd567db920776213d13
FreshDev <billyhans90@gmail.com>
2022-10-10 16:23:40 +0100

    Setup chat screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flash_chat/screens/welcome_screen.dart';
-import 'package:flash_chat/screens/login_screen.dart';
-import 'package:flash_chat/screens/registration_screen.dart';
-import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flashchat/screens/welcome_screen.dart';
+import 'package:flashchat/screens/login_screen.dart';
+import 'package:flashchat/screens/registration_screen.dart';
+import 'package:flashchat/screens/chat_screen.dart';
 
 void main() => runApp(FlashChat());
 
