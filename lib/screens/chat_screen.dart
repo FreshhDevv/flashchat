5a0f59a2c5a9839cab5708477cb226ed621a737e
FreshDev <billyhans90@gmail.com>
2022-10-20 15:34:37 +0100

    Learned more about streams

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -36,25 +36,6 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
-  // void getMessages() async {
-  //   final messages = await _firestore
-  //       .collection('messages')
-  //       .snapshots()
-  //       .map((snapshot) => snapshot.docs.map((doc) => doc.data()).toList());
-  //   print(messages);
-  // }
-  // void getMessages() => _firestore
-  //     .collection('messages')
-  //     .snapshots()
-  //     .map((snapshot) => snapshot.docs.map((doc) => doc.data()).toList());
-
-  // void getMessages() async {
-  //   QuerySnapshot messages = await _firestore.collection('messages').get();
-  //   for (var message in messages.docs) {
-  //     print(message.data());
-  //   }
-  // }
-
   void messagesStream() async {
     await for (var snapshot in _firestore.collection('messages').snapshots()) {
       for (var message in snapshot.docs) {
